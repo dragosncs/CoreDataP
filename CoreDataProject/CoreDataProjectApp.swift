f8769d755197cbc453d66fdda01d39f08add691f
Dragos Neacsu <dragosncs@gmail.com>
2023-08-21 15:55:02 +0300

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Dragos Neacsu on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
